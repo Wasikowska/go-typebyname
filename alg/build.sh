b8df8ea6de54a1ffafdc039448dc2083e304168d
Zhujy <zhujy@Cirilla.yan>
2021-02-20 01:00:23 +0800

    util: add valgrind support

--- alg/build.sh
+++ alg/build.sh
@@ -1,5 +1,43 @@
 
-if [[ $1 = "build" || -z $1 ]]; then
+# the leading colon means SILENT error reporting mode
+optstring=":gh"
+valgrind_friendly=0
+
+while getopts ${optstring} arg; do
+    case ${arg} in
+	h)
+	    echo "usage: build.sh [options] [cmd]"
+	    echo "  options:"
+	    echo "    -g: create valgrind-friendly executable"
+	    echo "    -h: display this help message"
+	    echo "  cmd:"
+	    echo "    build: build executable. this is the default cmd"
+	    echo "    clean: remove output directory"
+	    exit 0
+	    ;;
+	g)
+	    valgrind_friendly=1
+	    ;;
+	:)
+	    echo "$0: Must supply an argument to -$OPTARG." >&2
+	    exit 1
+	    ;;
+	?)
+	echo "Invalid option: -${OPTARG}."
+	exit 2
+	;;
+    esac
+done
+
+# OPTIND is the index of the cmd
+if (( ${OPTIND} > $# )); then
+    cmd=build
+else
+    shift $(( ${OPTIND} - 1 ))
+    cmd=$1
+fi
+
+if [[ $cmd = "build" ]]; then
     # check dot
     if ! which dot > /dev/null 2>&1; then
 	echo "error: can not find graphviz tools!"
@@ -12,7 +50,14 @@ if [[ $1 = "build" || -z $1 ]]; then
 
     (
 	cd output
-	cmake -G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
+
+	cmake_args="-G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug"
+	if (( $valgrind_friendly == 1 )); then
+	    cmake ${cmake_args} -DCMAKE_CXX_FLAGS="-O1 -Wall"
+	else
+	    cmake ${cmake_args}
+	fi
+
 	cmake --build .
     )
 
@@ -22,8 +67,11 @@ if [[ $1 = "build" || -z $1 ]]; then
     if [[ ! -e alg ]]; then
 	ln -s ./output/alg alg
     fi
-elif [[ $1 = "clean" ]]; then
+elif [[ cmd = "clean" ]]; then
     if [[ -d ./output ]]; then
 	rm -rf ./output
     fi
+else
+    echo "Invalid cmd: ${cmd}"
+    exit 3
 fi
